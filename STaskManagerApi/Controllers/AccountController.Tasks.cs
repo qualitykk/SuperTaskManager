7a7122158c956f32fc539974eaf71129adb49bb8
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-05-16 16:24:49 +0200

    Move Models to Library

---
+++ STaskManagerApi/Controllers/AccountController.Tasks.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net;
+using STaskManagerLibrary;
+
+namespace STaskManagerApi.Controllers
+{
+    public partial class AccountController
+    {
+        private DbSet<STask> _tasks => _context.Task;
+
+        [HttpGet("{user}/tasks")]
+        public ActionResult<List<STask>> GetTasks(int user)
+        {
+            if (_context == null)
+                return NotFound();
+
+            var tasks = _tasks.Where(t => t.Account == user).ToList();
+            return Ok(tasks);
+        }
+        private STask? TaskById(int user, int task)
+        {
+            if (_tasks == null)
+                return null;
+
+            return _tasks.Where(t => t.Account == user && t.Tid == task)?.FirstOrDefault(); ;
+        }
+
+        [HttpGet("{user}/tasks/{taskid}")]
+        public ActionResult<STask> GetTask(int user, int taskid)
+        {
+            if (_context == null)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            var task = TaskById(user, taskid);
+            if (task == null)
+                return NotFound();
+
+            return Ok(task);
+        }
+
+        [HttpPost("{user}/tasks")]
+        public async Task<ActionResult> PostTask(int user, [FromBody] STask t)
+        {
+            if (_context == null)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            int newid = _tasks.Max(t => t.Tid) + 1;
+            t.Tid = newid;
+
+            await _tasks.AddAsync(t);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTask), new { user = user, taskid = newid }, t);
+        }
+
+        [HttpPatch("{user}/tasks/{taskid}")]
+        public async Task<ActionResult> PatchTask(int user, int taskid, [FromBody] STask info)
+        {
+            if (_context == null)
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            var task = TaskById(user, taskid);
+            if (task == null)
+                return NotFound();
+
+            task.Name = info.Name;
+            task.Description = info.Description;
+            task.Priority = info.Priority;
+
+            task.Belongsto = info.Belongsto;
+            task.Required = info.Required;
+            task.Duedate = info.Duedate;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ STaskManagerApi/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace STaskManagerApi.Controllers
+{
+    [Route("api/accounts/")]
+    [ApiController]
+    public partial class AccountController : ControllerBase
+    {
+        private static TaskManagerContext _context = new();
+    }
+}
