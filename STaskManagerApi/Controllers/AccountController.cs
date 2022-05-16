0f0e9915a07a43d3938d8a94e756861c5a4bf7af
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-05-16 16:24:49 +0200

    DB Reverse Engineer

---
+++ STaskManagerApi/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace STaskManagerApi.Controllers
+{
+    [Route("api/áccounts/")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private static TaskManagerContext _context = new();
+        [HttpGet("{user}/tasks")]
+        public ActionResult<List<Task>> GetTasks(int user)
+        {
+            if(_context == null)
+                return NotFound();
+
+            return Ok(_context.Task.Where(t => t.Account == user).ToList());
+        }
+    }
+}
