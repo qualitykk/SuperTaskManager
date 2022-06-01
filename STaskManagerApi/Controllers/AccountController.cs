3620bd4df2120cb74d5c0886ac6e2db16aff7efa
Kilian <57487634+qualitycont@users.noreply.github.com>
2022-06-01 09:57:58 +0200

    Basic User Service

--- STaskManagerApi/Controllers/AccountController.cs
+++ STaskManagerApi/Controllers/AccountController.cs
@@ -3,19 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace STaskManagerApi.Controllers
 {
-    [Route("api/accounts/")]
     [ApiController]
-    public partial class AccountController : ControllerBase
     {
         private static TaskManagerContext _context = new();
-        private static bool IsTaskDataLoaded()
         {
-            return _context != null;
-        }
 
-        private static bool IsAccountDataLoaded()
-        {
-            return Service.Auth.IsActive();
         }
     }
 }
