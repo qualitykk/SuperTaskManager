3c65cd29d8357ff5585d4e32b9def6d9e2d791cc
Kilian <noqualitycont@gmail.com>
2022-06-11 22:29:48 +0200

    More features on login window

--- STaskManagerClient/API.cs
+++ STaskManagerClient/API.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace STaskManagerClient
@@ -10,16 +12,45 @@ namespace STaskManagerClient
     internal static class API
     {
         private static int userId;
-        private static string? auth;
+        private static long auth;
+
+        private const string ApiUrl = "https://localhost:7242/api/";
+        private static HttpClient client = new HttpClient() { BaseAddress = new Uri(ApiUrl) };
+        private static JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
         /// <summary>
         /// Logs in with username and hashed password
         /// </summary>
-        /// <param name="user"></param>
-        /// <param name="password"></param>
+        /// <param name="user">Username</param>
+        /// <param name="password">Hashed password</param>
         /// <returns>Success</returns>
         public static async Task<bool> LoginAsync(string user, string password)
         {
-            throw new NotImplementedException();
+            LoginRequest request = new(user, password);
+            var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+
+            try
+            {
+                var answer = await client.PostAsync("/login", content);
+                if (!answer.IsSuccessStatusCode)
+                    return false;
+
+                var response = JsonSerializer.Deserialize<LoginResponse>(await answer.Content.ReadAsStringAsync(), options);
+                if (!response)
+                    return false;
+
+#nullable disable
+                userId = (int)response.Userid;
+                auth = (long)response.Auth;
+#nullable restore
+
+                return true;
+            }
+            catch(HttpRequestException)
+            {
+
+            }
+            return false;
         }
 
         public static async Task<List<STask>> GetTasksAsync()
--- STaskManagerClient/Login.xaml.cs
+++ STaskManagerClient/Login.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
 
 namespace STaskManagerClient
 {
@@ -19,9 +20,36 @@ namespace STaskManagerClient
     /// </summary>
     public partial class Login : Window
     {
+        private static SHA512 hash = SHA512.Create();
         public Login()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string user = tbUsername.Text;
+            string password = tbPassword.Password;
+            if(string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("No username and password specified!");
+                return;
+            }
+
+            // var result = true;
+            var result = await API.LoginAsync(user, Convert.ToBase64String(hash.ComputeHash(Encoding.UTF8.GetBytes(password))));
+            if(!result)
+            {
+                MessageBox.Show("Incorrect password or username!");
+            }
+            else
+            {
+                MainWindow window = new();
+                window.Show();
+                Application.Current.MainWindow = window;
+                Close();
+            }
+            return;
         }
     }
 }
